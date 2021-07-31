b37466ab9f54304aef8273b254fffadfae7e65bc
dilkushrajpurohit <dilkushpurohit963@gmail.com>
2021-07-30 20:00:05 -0700

    f

--- index.js
+++ index.js
@@ -9,12 +9,27 @@ var port = process.env.PORT || 3000;
  app.get("/class9",function(req,res){
  	res.sendfile("static/class9.html")
  })
+ app.get("/blog",function(req,res){
+  res.sendfile("static/blog.html")
+ })
  app.get("/Class10",function(req,res){
     res.sendfile("static/class10.html")
  })
  app.get("/login",function(req,res){
    res.sendfile("static/login.html")
  })
+
+
+
  app.listen(port,()=>{
- 	console.log('listening at port 4500')
+  console.log('listening at port 4500')
  })
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
--- static/app.js
+++ static/app.js
@@ -256,3 +256,62 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
+
+
+    // Using IIFE for Implementing Module Pattern to keep the Local Space for the JS Variables
+(function() {
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var serverUrl = "/",
+        comments = [],
+        pusher = new Pusher('<your-api-key>', {
+          cluster: 'ap2',
+          encrypted: true
+        }),
+        // Subscribing to the 'flash-comments' Channel
+        channel = pusher.subscribe('flash-comments'),
+        commentForm = document.getElementById('comment-form'),
+        commentsList = document.getElementById('comments-list'),
+        commentTemplate = document.getElementById('comment-template');
+
+    // Binding to Pusher Event on our 'flash-comments' Channel
+    channel.bind('new_comment',newCommentReceived);
+
+    // Adding to Comment Form Submit Event
+    commentForm.addEventListener("submit", addNewComment);
+
+    // New Comment Receive Event Handler
+    // We will take the Comment Template, replace placeholders & append to commentsList
+    function newCommentReceived(data){
+      var newCommentHtml = commentTemplate.innerHTML.replace('{{name}}',data.name);
+      newCommentHtml = newCommentHtml.replace('{{email}}',data.email);
+      newCommentHtml = newCommentHtml.replace('{{comment}}',data.comment);
+      var newCommentNode = document.createElement('div');
+      newCommentNode.classList.add('comment');
+      newCommentNode.innerHTML = newCommentHtml;
+      commentsList.appendChild(newCommentNode);
+    }
+
+    function addNewComment(event){
+      event.preventDefault();
+      var newComment = {
+        "name": document.getElementById('new_comment_name').value,
+        "email": document.getElementById('new_comment_email').value,
+        "comment": document.getElementById('new_comment_text').value
+      }
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", serverUrl+"comment", true);
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState != 4 || xhr.status != 200) return;
+
+        // On Success of creating a new Comment
+        console.log("Success: " + xhr.responseText);
+        commentForm.reset();
+      };
+      xhr.send(JSON.stringify(newComment));
+    }
+
+})();
